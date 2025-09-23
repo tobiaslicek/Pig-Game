e32db35e308de1c8f5c0b67c3cc38f71ab0381e9
Tobiáš Licek <licek@voatt.cz>
2025-09-23 20:32:33 +0200

    Implement dice rolling functionality and initialize game state

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use script';
+
+// Selecting elements
+
+// const score1 = document.querySelector('#score--0');
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // Rolling dice functionality
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. Display dice
+
+  // 3. Check for rolled 1
+});
