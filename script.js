3daeceb7d4cb380c17d71aaedb6e49392d25f96d
Tobiáš Licek <licek@voatt.cz>
2025-09-26 17:07:03 +0200

    Refactor dice rolling logic to use querySelector for score element and fix current score display for active player

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Selecting elements
 
 // const score1 = document.querySelector('#score--0');
-const score0El = document.getElementById('score--0');
+const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
@@ -18,21 +18,27 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-let currentScore = 0;
+const scores = [0, 0];
+let currentPlayer = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // Rolling dice functionality
   const dice = Math.trunc(Math.random() * 6) + 1;
+
   // 2. Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
   // 3. Check for rolled 1
   if (dice !== 1) {
     // add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; //Change late for different players
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
