97140fbf2d7c892beb38900db34c0329e35ec9df
Tobiáš Licek <licek@voatt.cz>
2025-09-27 09:33:06 +0200

    Refactor player element selection and update active player logic in dice rolling functionality

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 // Selecting elements
 
-// const score1 = document.querySelector('#score--0');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -39,6 +40,10 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     // switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
