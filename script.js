d9f5b93cd85f4200a24cfc5c8df788039249e126
Tobiáš Licek <licek@voatt.cz>
2025-10-03 20:43:00 +0200

    Refactor switchPlayer function for improved code reusability and clarity

--- script.js
+++ script.js
@@ -23,6 +23,14 @@ const scores = [0, 0];
 let currentPlayer = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // Rolling dice functionality
@@ -40,10 +48,20 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     // switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  // 1. Add current score to active player's score
+  scores[activePlayer] += currentScore;
+  // scores[1] = scores[1] + currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  // 2. Check if player's score is >= 100
+  // Finish the game
+
+  // Switch to the next player
+  switchPlayer();
 });
